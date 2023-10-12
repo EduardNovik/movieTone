7a618c8ef79b76abce4b1f57b9f4f8f4a23ffab6
Eduard <eddnovik@gmail.com>
2023-10-12 23:46:39 +0200

    add shadcn

--- apps/web/src/vite-env.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="vite/client" />
-
-declare interface ImportMetaEnv {
-  API: string;
-  AUTHORIZATION: string;
-}
--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -1,2 +1,9 @@
 // component exports
 export { Card } from "./card";
+export {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from './src/components/dropdown-menu'
+export { Button } from "./src/components/button";
