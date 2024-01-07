15be8ace242628ac0c139f29944f9d34e66be8cf
EduardNovik <eddnovik@gmail.com>
2024-01-07 21:05:25 +0100

    small fix

--- be/app/src/service/user.ts
+++ be/app/src/service/user.ts
@@ -3,6 +3,7 @@ import { db } from "../db.ts";
 import { users, identities, watchlists } from "@movieTone/database-schema";
 import { eq } from "drizzle-orm";
 import crypto from "crypto";
+import { SessionRequest } from "supertokens-node/framework/express";
 
 // getUserByIdentityIdService
 export async function getUserByIdentityIdService(identityId: string) {
@@ -14,8 +15,6 @@ export async function getUserByIdentityIdService(identityId: string) {
 
   return user;
 }
-
-
 
 // userOnboard----
 export async function userOnboard(
@@ -90,4 +89,20 @@ export async function getUserByIdentityId(
   res.status(200).json({ user: user });
   console.log(user);
   return user;
+}
+
+// getUserSessionData----
+
+export async function protectedContext(
+  req: SessionRequest,
+  res: Response,
+  app: Application
+) {
+  // userId here must be the same as identityId
+  let userId = req.session!.getUserId();
+  let identityId = app.locals.identityId;
+  res.json({
+    userId: userId,
+    identityId: identityId,
+  });
 }
