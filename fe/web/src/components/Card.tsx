ebede97a7e8c7991100443876b6425f3d89f0f7e
EduardNovik <eddnovik@gmail.com>
2024-08-12 00:01:57 +0200

    update

--- fe/web/src/components/Card.tsx
+++ fe/web/src/components/Card.tsx
@@ -1,72 +1,14 @@
 import starIcon from '../assets/iconfinder_star.png';
 import { useNavigate } from '@tanstack/react-router';
-import { Button, useToast } from '@movieTone/ui';
-import axios from 'axios';
-import { Plus, ScrollText } from 'lucide-react';
 
-import {
-  DropdownMenu,
-  DropdownMenuTrigger,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuShortcut,
-  DropdownMenuGroup,
-} from '@movieTone/ui';
-
-import useUserWatchlistsSWR from '../api/SWR/useUserWatchlistsSWR';
 import { useRef } from 'react';
-
+import AddToWatchlist from './AddToWatchlist';
 interface CardProps {
   item: Record<string, any>;
 }
 
-interface Watchlist {
-  id: string;
-  name: string;
-  ganre: string;
-  userId: string;
-}
-
 const Card = ({ item }: CardProps) => {
   const navigate = useNavigate();
-  const { toast } = useToast();
-  const { data } = useUserWatchlistsSWR();
-
-  const addTitleToWatchlist = async (watchlist: Watchlist) => {
-    try {
-      await axios.post(`${window.origin}/api/watchlist/addTitle`, {
-        id: item.id,
-        name: item.title ? item.title : item.name,
-        img: item.backdrop_path,
-        imdb: item.vote_average,
-        year: parseInt(item.release_date.replace(/-/g, '')),
-        description: item.overview,
-        watchlistid: watchlist.id,
-      });
-      toast({ title: 'Title added' });
-    } catch (error: any) {
-      toast({ variant: 'destructive', title: 'Something went wrong.' });
-      console.log(error);
-    }
-  };
-
-  // const addTitleAndCreateWatchlist = async (watchlist: Watchlist) => {
-  //   try {
-  //     await axios.post(`${window.origin}/api/watchlist/addTitle`, {
-  //       id: item.id,
-  //       name: item.title ? item.title : item.name,
-  //       img: item.backdrop_path,
-  //       imdb: item.vote_average,
-  //       year: parseInt(item.release_date.replace(/-/g, '')),
-  //       description: item.overview,
-  //       watchlistid: watchlist.id,
-  //     });
-  //     toast({ title: 'Title added' });
-  //   } catch (error: any) {
-  //     toast({ variant: 'destructive', title: 'Something went wrong.' });
-  //     console.log(error);
-  //   }
-  // };
 
   const ref = useRef<HTMLDivElement>(null);
 
@@ -85,67 +27,44 @@ const Card = ({ item }: CardProps) => {
   };
 
   return (
-    <div
-      className="content w-full flex flex-col items-center lg:w-1/3  ease-in-out duration-300 cursor-pointer hover:scale-105 p-3"
-      onMouseEnter={showCurtain}
-      onMouseLeave={hideCurtain}
-    >
-      <div className="relative dark:shadow-2xl hover:drop-shadow-2xl drop-shadow-md rounded-md dark:hover:shadow-myViolet ease-in-out duration-300">
-        <img
-          src={`https://www.themoviedb.org/t/p/original/${item.backdrop_path}`}
-          alt="cover_img"
-          className="rounded-lg"
-          loading="lazy"
-          onClick={() =>
-            navigate({ to: '/title/$id', params: { id: item.id } })
-          }
-        />
-        <p className="text-center absolute top-4 left-8 text-white">
-          {item.title ? item.title : item.name}
-        </p>
-        <span className="flex gap-2 absolute top-4 right-8 items-center">
-          <p className="text-yellow-500">
-            {Math.round(item.vote_average * 10) / 10}
+    <div>
+      <div
+        className="content w-full flex flex-col items-center lg:w-1/3  ease-in-out duration-300 cursor-pointer hover:scale-105 p-3"
+        onMouseEnter={showCurtain}
+        onMouseLeave={hideCurtain}
+      >
+        <div className="relative dark:shadow-2xl hover:drop-shadow-2xl drop-shadow-md rounded-md dark:hover:shadow-myViolet ease-in-out duration-300">
+          <img
+            src={`https://www.themoviedb.org/t/p/original/${item.backdrop_path}`}
+            alt="cover_img"
+            className="rounded-lg"
+            loading="lazy"
+            onClick={() =>
+              navigate({ to: '/title/$id', params: { id: item.id } })
+            }
+          />
+          <p className="text-center absolute top-4 left-8 text-white">
+            {item.title ? item.title : item.name}
           </p>
-          <img src={starIcon} alt="" className="w-4 h-4" />
-        </span>
-        <div
-          ref={ref}
-          className="opacity-0 absolute bottom-0 left-0 right-0 bg-black h-10 z-20 rounded-b-lg transition-all duration-300"
-        >
-          <div className="absolute inset-0 flex items-center justify-center">
-            <p className="text-white">{item.title ? item.title : item.name}</p>
+          <span className="flex gap-2 absolute top-4 right-8 items-center">
+            <p className="text-yellow-500">
+              {Math.round(item.vote_average * 10) / 10}
+            </p>
+            <img src={starIcon} alt="" className="w-4 h-4" />
+          </span>
+          <div
+            ref={ref}
+            className="opacity-0 absolute bottom-0 left-0 right-0 bg-black h-10 z-20 rounded-b-lg transition-all duration-300"
+          >
+            <div className="absolute inset-0 flex items-center justify-center">
+              <p className="text-white">
+                {item.title ? item.title : item.name}
+              </p>
+            </div>
           </div>
         </div>
-
-        {Array.isArray(data) && (
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button variant="outline" className="py-0 px-2">
-                <ScrollText size={15} />
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent className="max-w-xs">
-              <DropdownMenuGroup>
-                {data?.map((watchlist: Watchlist) => {
-                  return (
-                    <DropdownMenuItem
-                      key={watchlist.id}
-                      className="cursor-pointer hover:bg-gray-200"
-                      onClick={() => addTitleToWatchlist(watchlist)}
-                    >
-                      {watchlist.name}
-                      <DropdownMenuShortcut>
-                        <Plus size={15} />
-                      </DropdownMenuShortcut>
-                    </DropdownMenuItem>
-                  );
-                })}
-              </DropdownMenuGroup>
-            </DropdownMenuContent>
-          </DropdownMenu>
-        )}
       </div>
+      <AddToWatchlist />
     </div>
   );
 };
--- fe/web/src/components/TitleTrailer.tsx
+++ fe/web/src/components/TitleTrailer.tsx
@@ -1,34 +1,45 @@
 import useTrailerByIdSWR from '../api/SWR/useTrailerByIdSWR';
-import TitleSkeleton from './skeletons/TitleSkeleton';
+import TrailerSkeleton from './skeletons/TrailerSkeleton';
+import Slider from 'react-slick';
 
 const TitleDetails = ({ id }: { id: string }) => {
-  console.log(id);
-
   const { data, error, isLoading } = useTrailerByIdSWR(id);
-  console.log(data);
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
+  if (error) {
+    return (
+      <div className="mt-[100px] flex flex-col justify-center items-center gap-6 relative">
+        {error}
+      </div>
+    );
+  }
 
   return (
-    <div className="mt-[100px] flex flex-col justify-center items-center gap-6 relative">
-      {isLoading ? (
-        <TitleSkeleton numOfCards={1} />
-      ) : data ? (
-        <div className="flex flex-col gap-2">
-          {data?.results.slice(0, 3).map((trailer: Record<string, any>) => (
-            <div key={trailer.id}>
+    <Slider {...settings} className="w-full">
+      {isLoading
+        ? Array(3)
+            .fill(0)
+            .map((_, i) => <TrailerSkeleton key={i} />)
+        : data?.results
+            .slice(0, 3)
+            .map((trailer: Record<string, any>) => (
               <iframe
-                width="560"
-                height="315"
+                key={trailer.id}
+                className="max-w-full aspect-video"
                 src={`https://www.youtube.com/embed/${trailer.key}`}
                 title={trailer.name}
                 allowFullScreen
+                loading="lazy"
               ></iframe>
-            </div>
-          ))}
-        </div>
-      ) : (
-        <div>{error}</div>
-      )}
-    </div>
+            ))}
+    </Slider>
   );
 };
 
